593f90ae73286924513d9ccface1c189b0336ab5
soo <ella1118@naver.com>
2021-11-06 23:34:55 +0900

    add js

---
+++ test/01_basic.js
@@ -0,0 +1,9 @@
+import * as THREE from 'C:\Users\SHANE\Desktop\cording\three.js practice\three.js-practice\node_modules\three\build\three.module.js';
+
+class App{
+    constructor() ()
+}
+
+window.onload = function(){
+    new App();
+}
